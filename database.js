6e8b62766b9ddb2ebff4e137e54b11e854a5b530
chrisephoto <chrisephotography@gmail.com>
2022-04-29 12:37:46 -0400

    Create database.js

---
+++ database.js
@@ -0,0 +1,171 @@
+series = [
+	{
+		title: "Super GT",
+		event: [
+			{
+				round: "Round 1",
+				date: "Apr 16 2022",
+				track: "Okayama",
+				url: "https://supergt.net/races/index/2022/Round1/",
+				image: "https://www.motoracing-japan.com/circuit/image/ti.gif"
+			},
+			{
+				round: "Round 2",
+				date: "May 3 2022",
+				track: "Fuji",
+				url: "https://supergt.net/races/index/2022/Round2/",
+				image: "https://www.motoracing-japan.com/circuit/image/fisco.gif"
+			},
+			{
+				round: "Round 3",
+				date: "May 28 2022",
+				track: "Suzuka",
+				url: "https://supergt.net/races/index/2022/Round3/",
+				image: "https://www.motoracing-japan.com/circuit/image/ap.gif"
+			},
+			{
+				round: "Round 4",
+				date: "Aug 6 2022",
+				track: "Fuji",
+				url: "https://supergt.net/races/index/2022/Round4/",
+				image: "https://www.motoracing-japan.com/circuit/image/fisco.gif"
+			},
+			{
+				round: "Round 5",
+				date: "Aug 27 2022",
+				track: "Suzuka",
+				url: "https://supergt.net/races/index/2022/Round5/",
+				image: "https://www.motoracing-japan.com/circuit/image/suzuka.gif"
+			},
+			{
+				round: "Round 6",
+				date: "Sep 17 2022",
+				track: "Sugo",
+				url: "https://supergt.net/races/index/2022/Round6/",
+				image: "https://www.motoracing-japan.com/circuit/image/sugo.gif"
+			},
+			{
+				round: "Round 7",
+				date: "Oct 1 2022",
+				track: "Autopolis",
+				url: "https://supergt.net/races/index/2022/Round7/",
+				image: "https://www.motoracing-japan.com/circuit/image/ap.gif"
+			},
+			{
+				round: "Round 8",
+				date: "Nov 5 2022",
+				track: "Motegi",
+				url: "https://supergt.net/races/index/2022/Round8/",
+				image: "https://www.motoracing-japan.com/circuit/image/motegi.gif"
+			}
+		]
+	},
+	{
+		title: "Super Taikyu",
+		event: [
+			{
+				round: "Round 1",
+				date: "March 19 2022",
+				track: "Suzuka",
+				url: "https://google.com/",
+				image: "https://www.motoracing-japan.com/circuit/image/suzuka.gif"
+			},
+			{
+				round: "Round 2",
+				date: "June 3 2022",
+				track: "Fuji",
+				url: "https://google.com/",
+				image: "https://www.motoracing-japan.com/circuit/image/fisco.gif"
+			},
+			{
+				round: "Round 3",
+				date: "July 9 2022",
+				track: "Sugo",
+				url: "https://google.com/",
+				image: "https://www.motoracing-japan.com/circuit/image/sugo.gif"
+			},
+			{
+				round: "Round 4",
+				date: "July 30 2022",
+				track: "Autopolis",
+				url: "https://google.com/",
+				image: "https://www.motoracing-japan.com/circuit/image/ap.gif"
+			},
+			{
+				round: "Round 5",
+				date: "September 3 2022",
+				track: "Motegi",
+				url: "https://google.com/",
+				image: "https://www.motoracing-japan.com/circuit/image/motegi.gif"
+			},
+			{
+				round: "Round 6",
+				date: "October 15 2022",
+				track: "Okayama",
+				url: "https://google.com/",
+				image: "https://www.motoracing-japan.com/circuit/image/ti.gif"
+			},
+			{
+				round: "Round 7",
+				date: "November 26 2022",
+				track: "Suzuka",
+				url: "https://google.com/",
+				image: "https://www.motoracing-japan.com/circuit/image/suzuka.gif"
+			}
+		]
+	},
+	{
+		title: "Super Formula",
+		event: [
+			{
+				round: "Round 1",
+				date: "March 19 2022",
+				track: "Suzuka",
+				url: "https://google.com/",
+				image: "https://cdn-1.motorsport.com/images/amp/27vJ5Xm0/s1000/32-orc-rookie-corolla-h2-conce.jpg"
+			},
+			{
+				round: "Round 2",
+				date: "June 3 2022",
+				track: "Fuji",
+				url: "https://google.com/",
+				image: "https://www.dailysportscar.com/wp-content/uploads/2020/05/Team-Lark-McLaren-F1-GTR-2.jpg"
+			},
+			{
+				round: "Round 3",
+				date: "July 9 2022",
+				track: "Sugo",
+				url: "https://google.com/",
+				image: "https://www.dailysportscar.com/wp-content/uploads/2020/05/Team-Lark-McLaren-F1-GTR-2.jpg"
+			},
+			{
+				round: "Round 4",
+				date: "July 30 2022",
+				track: "Autopolis",
+				url: "https://google.com/",
+				image: "https://www.dailysportscar.com/wp-content/uploads/2020/05/Team-Lark-McLaren-F1-GTR-2.jpg"
+			},
+			{
+				round: "Round 5",
+				date: "September 3 2022",
+				track: "Motegi",
+				url: "https://google.com/",
+				image: "https://www.dailysportscar.com/wp-content/uploads/2020/05/Team-Lark-McLaren-F1-GTR-2.jpg"
+			},
+			{
+				round: "Round 6",
+				date: "October 15 2022",
+				track: "Okayama",
+				url: "https://google.com/",
+				image: "https://www.dailysportscar.com/wp-content/uploads/2020/05/Team-Lark-McLaren-F1-GTR-2.jpg"
+			},
+			{
+				round: "Round 7",
+				date: "November 26 2022",
+				track: "Suzuka",
+				url: "https://google.com/",
+				image: "https://www.dailysportscar.com/wp-content/uploads/2020/05/Team-Lark-McLaren-F1-GTR-2.jpg"
+			}
+		]
+	}
+];
