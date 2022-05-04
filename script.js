3fb56e434a41091da52d2c4e3307bffe3201de8b
Chris <chrisephotography@gmail.com>
2022-05-04 12:19:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ for (let i = 0; i < seriesList.length; i++) {
 // listeners
 window.addEventListener("DOMContentLoaded", init);
 window.addEventListener("scroll", function() {
-    carouselOpacity(window.scrollY);
+  carouselOpacity(window.scrollY);
 }, false);
 
 
 // functions
 function init() {
   nextEvent();
-  createSlider(allEvents,"following");
+  createSlider(allEvents, "following");
   for (i = 0; i < seriesList.length; i++) {
     createSlider(eventList[i]);
   }
@@ -29,9 +29,9 @@ function createSlider(data, opt1, opt2, opt3, opt4) {
   //data - array of events
   //opt 1 - string = override heading
   //opt 2 - hide = hide past events
-	
-  opt1 = (typeof opt1 !== 'undefined') ?  opt1 : data[0].series;
-  
+
+  opt1 = (typeof opt1 !== 'undefined') ? opt1 : data[0].series;
+
   const main = document.getElementById("main");
   const section = document.createElement("section");
   const sliderOuter = document.createElement("div");
@@ -45,7 +45,7 @@ function createSlider(data, opt1, opt2, opt3, opt4) {
   const buttonLeft = document.createElement("a");
   buttonLeft.className = "nav-left";
   buttonLeft.setAttribute("onclick", "scrollSlider('slider-" + opt1 + "','l')");
-  
+
   main.appendChild(section);
   section.appendChild(sliderTitle);
   sliderTitle.appendChild(document.createTextNode(opt1))
@@ -54,89 +54,81 @@ function createSlider(data, opt1, opt2, opt3, opt4) {
   sliderMid.appendChild(sliderInner);
   sliderMid.appendChild(sliderInner);
   sliderInner.appendChild(buttonLeft);
-  
-  for (let j = 0; j < data.length; j++) {
-    const anchor = document.createElement("a");
-    anchor.className = "card";
-    anchor.href = data[j].url;
-    anchor.target = "_blank";
-    const img = document.createElement("img");
-    img.src = data[j].image;
-    const textdiv = document.createElement("div");
-    const series = document.createElement("p");
-    const round = document.createElement("p");
-    const date = document.createElement("p");
-    const track = document.createElement("p");
-    const seriestext = document.createTextNode(data[j].series);
-    const roundtext = document.createTextNode(data[j].round);
-    const datetext = document.createTextNode(data[j].date);
-    const tracktext = document.createTextNode(data[j].track);
-    sliderInner.appendChild(anchor);
-    anchor.appendChild(img);
-    textdiv.appendChild(series);
-    series.className = "text-size-xl";
-    series.appendChild(seriestext);
-    anchor.appendChild(textdiv);
-    textdiv.appendChild(round);
-    round.className = "text-size-xl";
-    round.appendChild(roundtext);
-    textdiv.appendChild(track);
-    track.className = "text-size-l";
-    track.appendChild(tracktext);
-    textdiv.appendChild(date);
-    date.className = "text-size-m";
-    date.appendChild(datetext);
-    if (Date.parse(data[j].date) < Date.parse(Date())) {
-      anchor.classList.add("unavailable");
-    }
-  }
-  const buttonRight = document.createElement("a");
-  buttonRight.className = "nav-right";
-  buttonRight.setAttribute("onclick", "scrollSlider('slider-" + opt1 + "','r')");
-  
-  sliderInner.appendChild(buttonRight);
-}
 
-function nextEvent() {
-  for (let i = 0; i < allEvents.length; i++) {
-    proposedDate = Date.parse(allEvents[i].date);
-    currentDate = Date.parse(Date()); timeDif=proposedDate - currentDate;
-    if (timeDif>= 0) {
-      if (timeDif < timeDifMin) {
-        timeDifMin = timeDif;
-        nextEvent = i;
+  for (let j = 0; j < data.length; j++) {
+
+    if (Date.parse(data[j].date) < Date.parse(Date()) AND opt2 == "hide") {
+
+    } else {
+
+      const anchor = document.createElement("a");
+      anchor.className = "card";
+      anchor.href = data[j].url;
+      anchor.target = "_blank";
+      if (Date.parse(data[j].date) < Date.parse(Date()) {
+          anchor.classList.add("unavailable");
+        }
+        const img = document.createElement("img"); img.src = data[j].image;
+        const textdiv = document.createElement("div");
+        const series = document.createElement("p");
+        const round = document.createElement("p");
+        const date = document.createElement("p");
+        const track = document.createElement("p");
+        const seriestext = document.createTextNode(data[j].series);
+        const roundtext = document.createTextNode(data[j].round);
+        const datetext = document.createTextNode(data[j].date);
+        const tracktext = document.createTextNode(data[j].track);
+
+
+        sliderInner.appendChild(anchor); anchor.appendChild(img); textdiv.appendChild(series); series.className = "text-size-xl"; series.appendChild(seriestext); anchor.appendChild(textdiv); textdiv.appendChild(round); round.className = "text-size-xl"; round.appendChild(roundtext); textdiv.appendChild(track); track.className = "text-size-l"; track.appendChild(tracktext); textdiv.appendChild(date); date.className = "text-size-m"; date.appendChild(datetext);
       }
     }
+    const buttonRight = document.createElement("a");
+    buttonRight.className = "nav-right";
+    buttonRight.setAttribute("onclick", "scrollSlider('slider-" + opt1 + "','r')");
+
+    sliderInner.appendChild(buttonRight);
   }
-  document.getElementById("carousel-header").innerHTML = allEvents[nextEvent].series;
-  document.getElementById("carousel-copy").innerHTML = allEvents[nextEvent].track;
-  document.getElementById("carousel-button").href = allEvents[nextEvent].url;
-  document.getElementById("carousel-image").src = allEvents[nextEvent].image;
-}
 
-function scrollSlider(id, direction) {
-	if (direction == "l") {
-		i = -1;
-	}
-	else if (direction == "r") {
-		i = 1;
-	}
-	else {
-		i = 0;
-	}
-	vw = window.innerWidth;
-	//count = getComputedStyle(document.querySelector(":root")).getPropertyValue('--card-count');
-	//scrollIncrement = vw / count;
-	document.getElementById(id).scrollBy({
-		top: 0,
-		left: vw * i,
-		behavior: "smooth"
-	});
-}
+  function nextEvent() {
+    for (let i = 0; i < allEvents.length; i++) {
+      proposedDate = Date.parse(allEvents[i].date);
+      currentDate = Date.parse(Date());
+      timeDif = proposedDate - currentDate;
+      if (timeDif >= 0) {
+        if (timeDif < timeDifMin) {
+          timeDifMin = timeDif;
+          nextEvent = i;
+        }
+      }
+    }
+    document.getElementById("carousel-header").innerHTML = allEvents[nextEvent].series;
+    document.getElementById("carousel-copy").innerHTML = allEvents[nextEvent].track;
+    document.getElementById("carousel-button").href = allEvents[nextEvent].url;
+    document.getElementById("carousel-image").src = allEvents[nextEvent].image;
+  }
 
-function carouselOpacity(sp) {
-  vh = window.innerHeight;
-  //n = 1 - (2 * sp) / vh; //current position in vh
-  n = 1 - (1.3 * sp / vh); //current position in vh
-	document.getElementById("carousel").style.opacity = n;
-}
+  function scrollSlider(id, direction) {
+    if (direction == "l") {
+      i = -1;
+    } else if (direction == "r") {
+      i = 1;
+    } else {
+      i = 0;
+    }
+    vw = window.innerWidth;
+    //count = getComputedStyle(document.querySelector(":root")).getPropertyValue('--card-count');
+    //scrollIncrement = vw / count;
+    document.getElementById(id).scrollBy({
+      top: 0,
+      left: vw * i,
+      behavior: "smooth"
+    });
+  }
+
+  function carouselOpacity(sp) {
+    vh = window.innerHeight;
+    //n = 1 - (2 * sp) / vh; //current position in vh
+    n = 1 - (1.3 * sp / vh); //current position in vh
+    document.getElementById("carousel").style.opacity = n;
+  }
