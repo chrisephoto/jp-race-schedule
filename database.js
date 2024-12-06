12f8dc9f8a76c4efe755df958f7db7bceede7414
Chris <chrisephotography@gmail.com>
2024-12-06 15:59:26 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,51 @@
+dataset2 = [
+  {
+    "series": "Super GT",
+    "logo": "images/super-gt.png",
+    "events": [
+      {"title": "Round 1", "date": "April 13", "track": "Okayama International Circuit"},
+      {"title": "Round 2", "date": "May 4", "track": "Fuji Speedway"},
+      {"title": "Round 3", "date": "June 28", "track": "Sepang International Circuit"},
+      {"title": "Round 4", "date": "August 3", "track": "Fuji Speedway"},
+      {"title": "Round 5", "date": "August 24", "track": "Suzuka Circuit"},
+      {"title": "Round 6", "date": "September 21", "track": "Sportsland Sugo"},
+      {"title": "Round 7", "date": "October 19", "track": "Autopolis"},
+      {"title": "Round 8", "date": "November 2", "track": "Mobility Resort Motegi"}
+    ]
+  },
+  {
+    "series": "Super Taikyu",
+    "logo": "images/super-taikyu.png",
+    "events": [
+      {"title": "Round 1", "date": "March 23", "track": "Mobility Resort Motegi"},
+      {"title": "Round 2", "date": "April 27", "track": "Suzuka Circuit"},
+      {"title": "Round 3", "date": "June 1", "track": "Fuji Speedway"},
+      {"title": "Round 4", "date": "July 6", "track": "Sportsland Sugo"},
+      {"title": "Round 5", "date": "July 27", "track": "Autopolis"},
+      {"title": "Round 6", "date": "October 26", "track": "Okayama International Circuit"},
+      {"title": "Round 7", "date": "November 16", "track": "Fuji Speedway"}
+    ]
+  },
+  {
+    "series": "Super Formula",
+    "logo": "images/super-formula.png",
+    "events": [
+      {"title": "Round 1", "date": "March 8", "track": "Suzuka Circuit"},
+      {"title": "Round 2", "date": "March 9", "track": "Suzuka Circuit"},
+      {"title": "Round 3", "date": "April 19", "track": "Mobility Resort Motegi"},
+      {"title": "Round 4", "date": "April 20", "track": "Mobility Resort Motegi"},
+      {"title": "Round 5", "date": "May 18", "track": "Autopolis"},
+      {"title": "Round 6", "date": "July 19", "track": "Fuji Speedway"},
+      {"title": "Round 7", "date": "July 20", "track": "Fuji Speedway"},
+      {"title": "Round 8", "date": "August 10", "track": "Sportsland Sugo"},
+      {"title": "Round 9", "date": "October 11", "track": "Fuji Speedway"},
+      {"title": "Round 10", "date": "October 12", "track": "Fuji Speedway"},
+      {"title": "Round 11", "date": "November 22", "track": "Suzuka Circuit"},
+      {"title": "Round 12", "date": "November 23", "track": "Suzuka Circuit"}
+    ]
+  }
+]
+
 dataset = [
   [
     // Super GT
