be8b9419fcb88d9c6d64799bccdb44048c9cf0c8
Chris <chrisephotography@gmail.com>
2024-12-06 19:01:28 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,47 +1,47 @@
 dataset = [
   {
     "series": "Super GT",
-    "logo": "images/super-gt.jpg",
+    "logo": "images/logo-super-gt.jpg",
     "events": [
-      {"title": "Round 1", "date": "2025-04-13", "track": "Okayama International Circuit"},
-      {"title": "Round 2", "date": "2025-05-04", "track": "Fuji Speedway"},
-      {"title": "Round 3", "date": "2025-06-28", "track": "Sepang International Circuit"},
-      {"title": "Round 4", "date": "2025-08-03", "track": "Fuji Speedway"},
-      {"title": "Round 5", "date": "2025-08-24", "track": "Suzuka Circuit"},
-      {"title": "Round 6", "date": "2025-09-21", "track": "Sportsland Sugo"},
-      {"title": "Round 7", "date": "2025-10-19", "track": "Autopolis"},
-      {"title": "Round 8", "date": "2025-11-02", "track": "Mobility Resort Motegi"}
+      {"title": "Round 1", "date": "2025-04-13", "track": "Okayama International Circuit", "image": "images/super-gt-okayama.jpg"},
+      {"title": "Round 2", "date": "2025-05-04", "track": "Fuji Speedway", "image": "images/super-gt-fuji.jpg"},
+      {"title": "Round 3", "date": "2025-06-28", "track": "Sepang International Circuit", "image": "images/super-gt-sepang.jpg"},
+      {"title": "Round 4", "date": "2025-08-03", "track": "Fuji Speedway", "image": "images/super-gt-fuji.jpg"},
+      {"title": "Round 5", "date": "2025-08-24", "track": "Suzuka Circuit", "image": "images/super-gt-suzuka.jpg"},
+      {"title": "Round 6", "date": "2025-09-21", "track": "Sportsland Sugo", "image": "images/super-gt-sugo.jpg"},
+      {"title": "Round 7", "date": "2025-10-19", "track": "Autopolis", "image": "images/super-gt-autopolis.jpg"},
+      {"title": "Round 8", "date": "2025-11-02", "track": "Mobility Resort Motegi", "image": "images/super-gt-motegi.jpg"}
     ]
   },
   {
     "series": "Super Taikyu",
-    "logo": "images/super-taikyu.jpg",
+    "logo": "images/logo-super-taikyu.jpg",
     "events": [
-      {"title": "Round 1", "date": "2025-03-23", "track": "Mobility Resort Motegi"},
-      {"title": "Round 2", "date": "2025-04-27", "track": "Suzuka Circuit"},
-      {"title": "Round 3", "date": "2025-06-01", "track": "Fuji Speedway"},
-      {"title": "Round 4", "date": "2025-07-06", "track": "Sportsland Sugo"},
-      {"title": "Round 5", "date": "2025-07-27", "track": "Autopolis"},
-      {"title": "Round 6", "date": "2025-10-26", "track": "Okayama International Circuit"},
-      {"title": "Round 7", "date": "2025-11-16", "track": "Fuji Speedway"}
+      {"title": "Round 1", "date": "2025-03-23", "track": "Mobility Resort Motegi", "image": "images/super-taikyu-motegi.jpg"},
+      {"title": "Round 2", "date": "2025-04-27", "track": "Suzuka Circuit", "image": "images/super-taikyu-suzuka.jpg"},
+      {"title": "Round 3", "date": "2025-06-01", "track": "Fuji Speedway", "image": "images/super-taikyu-fuji.jpg"},
+      {"title": "Round 4", "date": "2025-07-06", "track": "Sportsland Sugo", "image": "images/super-taikyu-sugo.jpg"},
+      {"title": "Round 5", "date": "2025-07-27", "track": "Autopolis", "image": "images/super-taikyu-autopolis.jpg"},
+      {"title": "Round 6", "date": "2025-10-26", "track": "Okayama International Circuit", "image": "images/super-taikyu-okayama.jpg"},
+      {"title": "Round 7", "date": "2025-11-16", "track": "Fuji Speedway", "image": "images/super-taikyu-fuji.jpg"}
     ]
   },
   {
     "series": "Super Formula",
-    "logo": "images/super-formula.jpg",
+    "logo": "images/logo-super-formula.jpg",
     "events": [
-      {"title": "Round 1", "date": "2025-03-08", "track": "Suzuka Circuit"},
-      {"title": "Round 2", "date": "2025-03-09", "track": "Suzuka Circuit"},
-      {"title": "Round 3", "date": "2025-04-19", "track": "Mobility Resort Motegi"},
-      {"title": "Round 4", "date": "2025-04-20", "track": "Mobility Resort Motegi"},
-      {"title": "Round 5", "date": "2025-05-18", "track": "Autopolis"},
-      {"title": "Round 6", "date": "2025-07-19", "track": "Fuji Speedway"},
-      {"title": "Round 7", "date": "2025-07-20", "track": "Fuji Speedway"},
-      {"title": "Round 8", "date": "2025-08-10", "track": "Sportsland Sugo"},
-      {"title": "Round 9", "date": "2025-10-11", "track": "Fuji Speedway"},
-      {"title": "Round 10", "date": "2025-10-12", "track": "Fuji Speedway"},
-      {"title": "Round 11", "date": "2025-11-22", "track": "Suzuka Circuit"},
-      {"title": "Round 12", "date": "2025-11-23", "track": "Suzuka Circuit"}
+      {"title": "Round 1", "date": "2025-03-08", "track": "Suzuka Circuit", "image": "images/super-formula-suzuka.jpg"},
+      {"title": "Round 2", "date": "2025-03-09", "track": "Suzuka Circuit", "image": "images/super-formula-suzuka.jpg"},
+      {"title": "Round 3", "date": "2025-04-19", "track": "Mobility Resort Motegi", "image": "images/super-formula-motegi.jpg"},
+      {"title": "Round 4", "date": "2025-04-20", "track": "Mobility Resort Motegi", "image": "images/super-formula-motegi.jpg"},
+      {"title": "Round 5", "date": "2025-05-18", "track": "Autopolis", "image": "images/super-formula-autopolis.jpg"},
+      {"title": "Round 6", "date": "2025-07-19", "track": "Fuji Speedway", "image": "images/super-formula-fuji.jpg"},
+      {"title": "Round 7", "date": "2025-07-20", "track": "Fuji Speedway", "image": "images/super-formula-fuji.jpg"},
+      {"title": "Round 8", "date": "2025-08-10", "track": "Sportsland Sugo", "image": "images/super-formula-sugo.jpg"},
+      {"title": "Round 9", "date": "2025-10-11", "track": "Fuji Speedway", "image": "images/super-formula-fuji.jpg"},
+      {"title": "Round 10", "date": "2025-10-12", "track": "Fuji Speedway", "image": "images/super-formula-fuji.jpg"},
+      {"title": "Round 11", "date": "2025-11-22", "track": "Suzuka Circuit", "image": "images/super-formula-suzuka.jpg"},
+      {"title": "Round 12", "date": "2025-11-23", "track": "Suzuka Circuit", "image": "images/super-formula-suzuka.jpg"}
     ]
   }
 ]
